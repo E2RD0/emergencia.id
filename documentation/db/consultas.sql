5c71304c66e0d640c953592c3bc61d881b1912f9
JosueGalRe <josuegalre@gmail.com>
2020-04-03 17:00:06 -0600

    db: consultas: Formateado de las consultas

--- documentation/db/consultas.sql
+++ documentation/db/consultas.sql
@@ -1,26 +1,87 @@
 --Consultas para reporte con rango de fechas:
-SELECT accion_bitacora.accion, bitacora_usuario.descripcion, bitacora_usuario.fecha, usuario.uid
-FROM bitacora_usuario INNER JOIN usuario ON bitacora_usuario.id_usuario = usuario.id_usuario INNER JOIN accion_bitacora ON bitacora_usuario.id_accion_bitacora = accion_bitacora.id_accion_bitacora
-WHERE bitacora_usuario.fecha BETWEEN '2020-01-01' AND '2020-02-01'
-GROUP BY bitacora_usuario.descripcion, bitacora_usuario.fecha, usuario.uid, accion_bitacora.accion
+SELECT
+    accion_bitacora.accion,
+    bitacora_usuario.descripcion,
+    bitacora_usuario.fecha,
+    usuario.uid
+FROM
+    bitacora_usuario
+    INNER JOIN usuario ON bitacora_usuario.id_usuario = usuario.id_usuario
+    INNER JOIN accion_bitacora ON bitacora_usuario.id_accion_bitacora = accion_bitacora.id_accion_bitacora
+WHERE
+    bitacora_usuario.fecha BETWEEN '2020-01-01'
+    AND '2020-02-01'
+GROUP BY
+    bitacora_usuario.descripcion,
+    bitacora_usuario.fecha,
+    usuario.uid,
+    accion_bitacora.accion;
 
-SELECT enlaces_compartir.enlace, enlaces_compartir.fecha_creacion, enlaces_compartir.fecha_expiracion, usuario.uid
-FROM enlaces_compartir INNER JOIN usuario ON enlaces_compartir.id_usuario = usuario.id_usuario
-WHERE enlaces_compartir.fecha_creacion BETWEEN '2020-01-01' AND '2020-02-01'
-GROUP BY enlaces_compartir.enlace, enlaces_compartir.fecha_creacion, enlaces_compartir.fecha_expiracion, usuario.uid
+SELECT
+    enlaces_compartir.enlace,
+    enlaces_compartir.fecha_creacion,
+    enlaces_compartir.fecha_expiracion,
+    usuario.uid
+FROM
+    enlaces_compartir
+    INNER JOIN usuario ON enlaces_compartir.id_usuario = usuario.id_usuario
+WHERE
+    enlaces_compartir.fecha_creacion BETWEEN '2020-01-01'
+    AND '2020-02-01'
+GROUP BY
+    enlaces_compartir.enlace,
+    enlaces_compartir.fecha_creacion,
+    enlaces_compartir.fecha_expiracion,
+    usuario.uid;
 
-SELECT usuario.id_usuario, procedimiento_medico.procedimiento, procedimiento_medico.fecha
-FROM procedimiento_medico INNER JOIN usuario ON procedimiento_medico.id_usuario = usuario.id_usuario
-WHERE procedimiento_medico.fecha BETWEEN '2020-01-01' AND '2020-02-01'
-GROUP BY usuario.id_usuario, procedimiento_medico.procedimiento, procedimiento_medico.fecha
-
+SELECT
+    usuario.id_usuario,
+    procedimiento_medico.procedimiento,
+    procedimiento_medico.fecha
+FROM
+    procedimiento_medico
+    INNER JOIN usuario ON procedimiento_medico.id_usuario = usuario.id_usuario
+WHERE
+    procedimiento_medico.fecha BETWEEN '2020-01-01'
+    AND '2020-02-01'
+GROUP BY
+    usuario.id_usuario,
+    procedimiento_medico.procedimiento,
+    procedimiento_medico.fecha;
 --Consultas para graficos
-SELECT COUNT(tipo_sangre) AS Cantidad, u.tipo_sangre AS "TIPO" FROM usuario u GROUP BY tipo_sangre
-SELECT COUNT(id_enlace) FROM enlaces_compartir e WHERE e.id_usuario = 1
-SELECT alergia FROM usuario_alergia GROUP BY alergia HAVING COUNT(*)>1
-SELECT COUNT(*) FROM usuario
-SELECT COUNT(*) FROM organizacion
+SELECT
+    COUNT(tipo_sangre) AS Cantidad,
+    u.tipo_sangre AS "TIPO"
+FROM
+    usuario u
+GROUP BY
+    tipo_sangre;
 
+SELECT
+    COUNT(id_enlace)
+FROM
+    enlaces_compartir e
+WHERE
+    e.id_usuario = 1;
+
+SELECT
+    alergia
+FROM
+    usuario_alergia
+GROUP BY
+    alergia
+HAVING
+    COUNT(*) > 1;
+
+SELECT
+    COUNT(*)
+FROM
+    usuario;
+
+SELECT
+    COUNT(*)
+FROM
+    organizacion;
 --Consultas para reportes con datos
 SELECT
     id_usuario AS "ID",
@@ -41,7 +102,7 @@ SELECT
 FROM
     usuario
 WHERE
-    di = '76529364-5'
+    di = '76529364-5';
 
 SELECT
     id_contacto AS "ID de contacto",
@@ -52,7 +113,7 @@ FROM
     contacto_emergencia
     INNER JOIN usuario ON contacto_emergencia.id_usuario = usuario.id_usuario
 WHERE
-    usuario.di = '76529364-5'
+    usuario.di = '76529364-5';
 
 SELECT
     id_seguro AS "No. de seguro",
@@ -64,7 +125,7 @@ FROM
     seguro_medico
     INNER JOIN usuario ON seguro_medico.id_usuario = usuario.id_usuario
 WHERE
-    usuario.di = '76529364-5'
+    usuario.di = '76529364-5';
 
 SELECT
     usuario.id_usuario AS "ID",
@@ -87,7 +148,7 @@ FROM
     usuario
     INNER JOIN seguro_medico ON seguro_medico.id_usuario = usuario.id_usuario
 WHERE
-    seguro_medico.compania = 'Seguros Azul'
+    seguro_medico.compania = 'Seguros Azul';
 
 SELECT
     id_procedimiento AS "No. de procedimiento",
@@ -108,4 +169,4 @@ FROM
     procedimiento_medico
     INNER JOIN usuario ON procedimiento_medico.id_usuario = usuario.id_usuario
 WHERE
-    usuario.di = '76529364-5'
+    usuario.di = '76529364-5';
