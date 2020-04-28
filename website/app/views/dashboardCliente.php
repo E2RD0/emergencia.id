c998f503fc4897a01e59a89961c5dda6c332f414
Cartagena2001 <43362524+Cartagena2001@users.noreply.github.com>
2020-04-28 17:35:40 -0600

    Update perfiles(modal compartir perfil)

--- website/app/views/dashboardCliente.php
+++ website/app/views/dashboardCliente.php
@@ -16,7 +16,11 @@ template::headerSite('Dashboard del cliente');
             <button type="button" class="btn btn-primary"><i class="fas fa-plus"></i> Nuevo perfil</button>
         </div>
     </div>
-    <hr>
+
+    <nav class="navbar navbar-expand-lg navbar-light ml-lg-3 ml-sm-0 prueba">
+        <a class="navbar-brand line" href="#">Mis perfiles</a>
+        <a class="navbar-brand line" href="#">Compartidos conmigo</a>
+    </nav>
 
     <!-- card de perfiles -->
     <div class="card mb-3 card mb-3 card border-0 card mb-5 mt-5" style="max-width: 540px; background-color: #F2F5FA;">
@@ -28,19 +32,87 @@ template::headerSite('Dashboard del cliente');
                 <div class="card-body">
                     <h5 class="card-title text-regular">Eduardo Estrada</h5>
                     <p class="card-text"><small class="text-muted">18 años, San salvador, El salvador.</small></p>
-                        <a href="" class="text-link mr-4" style="color:black"><i class="far fa-pencil"></i> Editar</a>
-                        <a href="" class="text-link mr-4" style="color:black"><i class="far fa-clone"></i> Duplicar</a>
-                        <a href="" class="text-link mr-4" style="color:black" id="dropdownMenuOffset" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-offset="0,5"><i class="fas fa-ellipsis-h"></i></a>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuOffset">
-                            <a class="dropdown-item" href="#"><i class="far fa-address-card mr-2"></i> Targeta QR</a>
-                            <a class="dropdown-item" href="#"><i class="far fa-arrow-alt-down mr-2"></i> Descargar PDF</a>
-                            <a class="dropdown-item" href="#"><i class="far fa-share-square mr-2"></i> Compartir</a>
-                            <a class="dropdown-item" href="#"><i class="far fa-trash mr-2"></i> Eliminar</a>
-                        </div>
+                    <a href="" class="text-link mr-4" style="color:black"><i class="far fa-pencil"></i> Editar</a>
+                    <a href="" class="text-link mr-4" style="color:black"><i class="far fa-clone"></i> Duplicar</a>
+                    <a href="" class="text-link mr-4" style="color:black" id="dropdownMenuOffset" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-offset="0,5"><i class="fas fa-ellipsis-h"></i></a>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuOffset">
+                        <a class="dropdown-item" href="#"><i class="far fa-address-card mr-2"></i> Targeta QR</a>
+                        <a class="dropdown-item" href="#"><i class="far fa-arrow-alt-down mr-2"></i> Descargar PDF</a>
+                        <a class="dropdown-item" href="#" type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter"><i class="far fa-share-square mr-2"></i> Compartir</a>
+                        <a class="dropdown-item" href="#"><i class="far fa-trash mr-2"></i> Eliminar</a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+
+    <!-- modal compartir perfil -->
+    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header  border-0">
+                    <h5 class="text-header-modal" id="exampleModalCenterTitle">Compartir el perfil</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p>Con este enlace pudes compartir la informacion del perfil médico para casos que no sean emergencias(compartirlo con tu doctor,etc).</p>
+                    <p class="card-text"><small class="text-muted">Copia el siguiente enlace</small></p>
+                    <div class="input-group mb-4">
+                        <input class="textfield" type="text" class="form-control" id="email" aria-describedby="basic-addon3">
+                        <div class="line"></div>
+                    </div>
+                </div>
+                <div class="modal-footer  border-0 d-flex justify-content-between">
+                    <small class="text-muted"><a href="" style="text-decoration: none;font-weight: 600;color:grey" data-toggle="modal" data-target="#compartirperfil" data-dismiss="modal">Compartir con un usuario</a></small>
+                    <button type="button" class="btn" style="background-color: white; color: #2F8DEB; font-weight: 600;">Abrir enclace</button>
+                    <button type="button" class="btn btn-primary" style="font-weight: 600;">Copiar enlace</button>  
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- modal compartir perfil con usuario -->
+    <div class="modal fade" id="compartirperfil" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header  border-0">
+                    <h5 class="text-header-modal" id="exampleModalCenterTitle">Compartir el perfil</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p>Puedes compartir el perfil médico con una persona en específico que sea un usuario de la plataforma.</p>
+                    <p class="card-text"><small class="text-muted">Correo electronico del usuario a compartir</small></p>
+                    <div class="input-group mb-4">
+                        <input class="textfield" type="text" class="form-control" id="email" aria-describedby="basic-addon3">
+                        <div class="line"></div>
+                    </div>
+                    <div class="d-flex justify-content-end">
+                        <button type="button" class="btn" style="font-weight: 600;color:black;border-color: #DDE3F0;">Invitar</button>  
+                    </div>
+                    <div>
+                        <p class="card-text"><small class="text-muted">Gestiona quién tiene acceso</small></p>
+                        <hr>
+                        <div class="d-flex justify-content-between">
+                            <p>Laura Navas</p>
+                            <p class="mr-5" style="color:grey">lauranavas@gmail.com</p>
+                            <i class="far fa-trash mt-1" style="color: grey"></i>
+                        </div>
+                        <hr>
+                    </div>
+                </div>
+                <div class="modal-footer  border-0 d-flex justify-content-between">
+                    <small class="text-muted"><a href="" style="text-decoration: none;font-weight: 600;color:grey" data-dismiss="modal" data-toggle="modal" data-target="#exampleModalCenter">Compartir enlace</a></small>
+                    <button type="button" class="btn btn-primary" style="font-weight: 600;" data-dismiss="modal">Aceptar</button>  
+                </div>
+            </div>
+        </div>
+    </div>
+
+
 
     <div class="card mb-3 card mb-3 card border-0 card mb-5" style="max-width: 540px;background-color: #F2F5FA;">
         <div class="row no-gutters">
@@ -52,14 +124,14 @@ template::headerSite('Dashboard del cliente');
                     <h5 class="card-title text-regular">José Roberto Estrada</h5>
                     <p class="card-text"><small class="text-muted">54 años, San salvador, El salvador.</small></p>
                     <a href="" class="text-link mr-4" style="color:black"><i class="far fa-pencil"></i> Editar</a>
-                        <a href="" class="text-link mr-4" style="color:black"><i class="far fa-clone"></i> Duplicar</a>
-                        <a href="" class="text-link mr-4" style="color:black" id="dropdownMenuOffset" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-offset="0,5"><i class="fas fa-ellipsis-h"></i></a>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuOffset">
+                    <a href="" class="text-link mr-4" style="color:black"><i class="far fa-clone"></i> Duplicar</a>
+                    <a href="" class="text-link mr-4" style="color:black" id="dropdownMenuOffset" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-offset="0,5"><i class="fas fa-ellipsis-h"></i></a>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuOffset">
                         <a class="dropdown-item" href="#"><i class="far fa-address-card mr-2"></i> Targeta QR</a>
-                            <a class="dropdown-item" href="#"><i class="far fa-arrow-alt-down mr-2"></i> Descargar PDF</a>
-                            <a class="dropdown-item" href="#"><i class="far fa-share-square mr-2"></i> Compartir</a>
-                            <a class="dropdown-item" href="#"><i class="far fa-trash mr-2"></i> Eliminar</a>
-                        </div>
+                        <a class="dropdown-item" href="#"><i class="far fa-arrow-alt-down mr-2"></i> Descargar PDF</a>
+                        <a class="dropdown-item" href="#" type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter"><i class="far fa-share-square mr-2"></i> Compartir</a>
+                        <a class="dropdown-item" href="#"><i class="far fa-trash mr-2"></i> Eliminar</a>
+                    </div>
                 </div>
             </div>
         </div>
