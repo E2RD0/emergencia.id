591b1566349bc3b95c1408bbd1c5aa9689bae58e
Cartagena2001 <43362524+Cartagena2001@users.noreply.github.com>
2020-07-29 23:39:38 -0600

    Show profiles

--- website/backend/models/PerfilesUsuario.php
+++ website/backend/models/PerfilesUsuario.php
@@ -17,6 +17,12 @@ class PerfilesUsuario
         return $db->rowCount();
     }
 
+    public function getShowProfile($param){
+        $db = new \Common\Database;
+        $db->query("SELECT perfil_medico.id_perfil_medico ,perfil_medico.nombres, perfil_medico.apellidos, pais.nombre, perfil_medico.ciudad, perfil_medico.foto FROM perfil_medico INNER JOIN pais ON perfil_medico.id_pais = pais.id_pais WHERE perfil_medico.id_usuario = :id") ;
+       $db->bind(':id', $param);
+        return $db->resultSet();
+    }
 }
 
 
---
+++ website/public/api/app/perfilUsuario.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once __DIR__ . '/../../../backend/init.php';
+require_once __DIR__ . '/../../../backend/controllers/ProfileUser.php';
+
+if (isset($_GET['action'])) {
+    #session_start();
+    $action = $_GET['action'];
+    $controller = new \ProfileUser;
+    $result = $controller->r;
+
+    switch ($action) {
+        case 'getshowProfile':
+            $result = $controller->getShowProfile();
+            break;
+        default:
+            \Common\Core::http404();
+    }
+    
+    header('content-type: application/json; charset=utf-8');
+	echo json_encode($result, JSON_PRETTY_PRINT);
+}
+else {
+    \Common\Core::http404();
+}
