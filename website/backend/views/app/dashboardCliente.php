7af3e4921e24341f05664de1f8f6e9e26c359d99
Cartagena2001 <43362524+Cartagena2001@users.noreply.github.com>
2020-08-27 18:25:37 -0600

    Website in English

--- website/backend/views/app/dashboardCliente.php
+++ website/backend/views/app/dashboardCliente.php
@@ -5,11 +5,11 @@ template::headerSite('Dashboard del cliente');
 
 <div id="dashboard">
     <div class="container mt-5" id="dashboard">
-        <h1 class="title-page">Perfiles</h1>
+        <h1 class="title-page">Profiles</h1>
 
         <div class="nav d-flex d-md-block line nav-profiles" role="tablist">
-            <a class="navbar-brand profile-option text-link active" data-toggle="tab" href="#perfiles">Mis perfiles</a>
-            <a class="navbar-brand profile-option text-link" data-toggle="tab" href="#compartidos">Compartidos conmigo</a>
+            <a class="navbar-brand profile-option text-link active" data-toggle="tab" href="#perfiles">My profiles</a>
+            <a class="navbar-brand profile-option text-link" data-toggle="tab" href="#compartidos">Shared with me</a>
             <!-- btn escritorio -->
             <button @click="createNewProfile" class="button button--small button-profile d-none d-md-block" role="button"><i class="fas fa-plus mr-2"></i>{{newProfile}}</button>
         </div>
@@ -27,27 +27,26 @@ template::headerSite('Dashboard del cliente');
                         </div>
                         <div class="col-sm-9">
                             <div class="card-body">
-                                <h5 class="card-title text-regular" v-if="item.nombres === null">Complete su nombre.</h5>
+                                <h5 class="card-title text-regular" v-if="item.nombres === null">Complete your name.</h5>
                                 <h5 class="card-title text-regular" v-else>{{item.nombres }} {{item.apellidos}}</h5>
                                 <p class="card-text" v-if="item.date_part === null">Complete la información.</p>
-                                <p class="card-text" v-else>{{item.date_part }} Años, {{item.nombre}}, {{item.ciudad}}</p>
+                                <p class="card-text" v-else>{{item.date_part }} Years, {{item.nombre}}, {{item.ciudad}}</p>
                                 <div class="d-flex flex-column d-sm-block">
                                     <a @click="redirectToEdit(item.id_perfil_medico)" class="color-text text-link mr-4 mb-3" style="cursor: pointer;">
                                         <i class="far fa-pencil mr-2" style="cursor: pointer"></i>
-                                        Editar</a>
+                                        Edit</a>
                                     <a href="#" class="color-text text-link mr-4 mb-3"><i class="far fa-clone mr-2"></i>
-                                        Duplicar</a>
+                                        Duplicate</a>
                                     <a href="#" class="color-text text-link mr-4 mb-3" id="dropdownMenuOffset" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-offset="0,5"><i class="fas fa-ellipsis-h"></i></a>
                                     <div class="dropdown-menu profile-more-options" aria-labelledby="dropdownMenuOffset">
                                         <a class="dropdown-item mb-1" href="#"><i class="far fa-address-card mr-2"></i>
                                             Tarjeta QR</a>
                                         <a class="dropdown-item mb-1" href="#"><i class="far fa-arrow-alt-down mr-2"></i>
-                                            Descargar PDF</a>
-                                        <a @click="getShares('id_perfil_medico', item.id_perfil_medico)" class="dropdown-item mb-1" href="#" type="button" class="btn btn-primary" data-toggle="modal" data-target="#compartirUsuarios"><i class="far fa-share-square mr-2"></i> Compartir</a>
-                                        <a class="dropdown-item mb-1" href="#"><i class="fas fa-history mr-2"></i></i> Ver
-                                            actividad</a>
+                                            Download PDF</a>
+                                        <a @click="getShares('id_perfil_medico', item.id_perfil_medico)" class="dropdown-item mb-1" href="#" type="button" class="btn btn-primary" data-toggle="modal" data-target="#compartirUsuarios"><i class="far fa-share-square mr-2"></i> Share</a>
+                                        <a class="dropdown-item mb-1" href="#"><i class="fas fa-history mr-2"></i></i> See activity </a>
                                         <a @click="encapsulateId('id_perfil_medico', item.id_perfil_medico)" class="dropdown-item" href="#" type="button" class="btn btn-primary" data-toggle="modal" data-target="#eliminarperfil">
-                                            <i class="far fa-trash mr-2"></i>Eliminar
+                                            <i class="far fa-trash mr-2"></i>Delete
                                         </a>
                                     </div>
                                 </div>
@@ -65,15 +64,15 @@ template::headerSite('Dashboard del cliente');
                 </div>
 
                 <div class="d-flex justify-content-between" v-else>
-                    <h4 class="mt-2">Página: {{pag}}</h4>
+                    <h4 class="mt-2">Page: {{pag}}</h4>
                     <nav aria-label="...">
                         <ul class="pagination">
                             <li class="page-item" v-show="pag != 1" @click.prevent="pag -= 1">
-                                <a class="button button--small" href="#">Atras</a>
+                                <a class="button button--small" href="#">Back</a>
                             </li>
 
                             <li class="page-item" v-show="pag * NUM_RESULTS / show.length < 1" @click.prevent="pag += 1">
-                                <a class="button button--small" href="#">Siguiente</a>
+                                <a class="button button--small" href="#">Next</a>
                             </li>
                         </ul>
                     </nav>
@@ -94,17 +93,17 @@ template::headerSite('Dashboard del cliente');
                                 <h5 class="card-title text-regular" v-if="item.nombres === null">Complete su nombre.</h5>
                                 <h5 class="card-title text-regular" v-else>{{item.nombres }} {{item.apellidos}}</h5>
                                 <p class="card-text" v-if="item.date_part === null">Complete la información.</p>
-                                <p class="card-text" v-else>{{item.date_part }} Años, {{item.nombre}}, {{item.ciudad}}</p>
+                                <p class="card-text" v-else>{{item.date_part }} Years, {{item.nombre}}, {{item.ciudad}}</p>
                                 <div class="d-flex flex-column d-sm-block">
                                     <a href="#" class="color-text text-link mr-4 mb-3"><i class="far fa-eye mr-2"></i>
-                                        Ver</a>
+                                        View</a>
                                     <a href="#" class="color-text text-link mr-4 mb-3"><i class="far fa-clone mr-2"></i>
-                                        Guardar</a>
+                                        Save</a>
                                     <a href="#" class="color-text text-link mr-4 mb-3" id="dropdownMenuOffset" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-offset="0,5"><i class="fas fa-ellipsis-h"></i></a>
                                     <div class="dropdown-menu profile-more-options" aria-labelledby="dropdownMenuOffset">
                                         <a class="dropdown-item mb-1" href="#"><i class="far fa-arrow-alt-down mr-2"></i>
-                                            Descargar PDF</a>
-                                        <a @click="encapsulateId('id_perfil_medico', item.id_perfil_medico)" class="dropdown-item" href="#" type="button" class="btn btn-primary" data-toggle="modal" data-target="#eliminarperfilcompartido"><i class="far fa-trash mr-2"></i> Eliminar</a>
+                                            Download PDF</a>
+                                        <a @click="encapsulateId('id_perfil_medico', item.id_perfil_medico)" class="dropdown-item" href="#" type="button" class="btn btn-primary" data-toggle="modal" data-target="#eliminarperfilcompartido"><i class="far fa-trash mr-2"></i> Delete</a>
                                     </div>
                                 </div>
                             </div>
@@ -123,15 +122,15 @@ template::headerSite('Dashboard del cliente');
                 </div>
 
                 <div class="d-flex justify-content-between" v-else>
-                    <h4 class="mt-2">Página: {{pagC}}</h4>
+                    <h4 class="mt-2">page: {{pagC}}</h4>
                     <nav aria-label="...">
                         <ul class="pagination">
                             <li class="page-item" v-show="pagC != 1" @click.prevent="pagC -= 1">
-                                <a class="button button--small" href="#">Atras</a>
+                                <a class="button button--small" href="#">Back</a>
                             </li>
 
                             <li class="page-item" v-show="pagC * NUM_RESULTSC / ShowShared.length < 1" @click.prevent="pagC += 1">
-                                <a class="button button--small" href="#">Siguiente</a>
+                                <a class="button button--small" href="#">Next</a>
                             </li>
                         </ul>
                     </nav>
@@ -179,15 +178,14 @@ template::headerSite('Dashboard del cliente');
             <div class="modal-dialog modal-dialog-centered" role="document">
                 <div class="modal-content">
                     <div class="modal-header  border-0">
-                        <h5 class="text-header-modal" id="exampleModalCenterTitle">Compartir el perfil</h5>
+                        <h5 class="text-header-modal" id="exampleModalCenterTitle">Share profile</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
                     <div class="modal-body">
-                        <p>Puedes compartir el perfil médico con una persona en específico: debe ser un usuario de la
-                            plataforma.</p>
-                        <p class="card-text"><small class="text-muted">Correo electrónico del usuario a compartir</small>
+                        <p>You can share the medical profile with a specific person: it must be a user of the platform.</p>
+                        <p class="card-text"><small class="text-muted">User's email to be shared</small>
                         </p>
                         <div class="input-group mb-4">
                             <input class="textfield" type="email" class="form-control" id="email" aria-describedby="basic-addon3" ref="email">
@@ -198,11 +196,11 @@ template::headerSite('Dashboard del cliente');
                                 <p class="text-left pl-3 mt-0 mr-auto" :class="textColor">{{sharingStatus}}</p>
                             </div>
                             <div class="col-3 text-right">
-                                <button @click="shareProfileWith()" type="button" class="btn" style="font-weight: 600;color:black;border-color: #DDE3F0;">Invitar</button>
+                                <button @click="shareProfileWith()" type="button" class="btn" style="font-weight: 600;color:black;border-color: #DDE3F0;">Invite</button>
                             </div>
                         </div>
                         <div>
-                            <p class="card-text" class="mb-3"><small class="text-muted">Gestiona quién tiene acceso</small></p>
+                            <p class="card-text" class="mb-3"><small class="text-muted">Manage who has access</small></p>
                             <div :class="'overflow-auto' + upperSpace" style="height:130px; max-height:130px;">
                                 <div class="d-flex justify-content-center" v-if="!sharedWith.length" v-html="loading"></div>
                                 <div v-else="sharedWith.length" v-cloak id="users" class="p-4 d-flex justify-content-between sharedProfileUser" v-for="user in sharedWith">
@@ -214,9 +212,8 @@ template::headerSite('Dashboard del cliente');
                         </div>
                     </div>
                     <div class="modal-footer  border-0 d-flex justify-content-between">
-                        <small class="text-muted"><a href="" style="text-decoration: none;font-weight: 600;color:grey" data-dismiss="modal" data-toggle="modal" data-target="#exampleModalCenter">Compartir
-                                enlace</a></small>
-                        <button type="button" class="btn btn-primary" style="font-weight: 600;" data-dismiss="modal">Aceptar</button>
+                        <small class="text-muted"><a href="" style="text-decoration: none;font-weight: 600;color:grey" data-dismiss="modal" data-toggle="modal" data-target="#exampleModalCenter">Share link</a></small>
+                        <button type="button" class="btn btn-primary" style="font-weight: 600;" data-dismiss="modal">Accept</button>
                     </div>
                 </div>
             </div>
@@ -230,19 +227,19 @@ template::headerSite('Dashboard del cliente');
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header border-0">
-                    <h5 class="text-header-modal" id="exampleModalCenterTitle">Eliminar perfil</h5>
+                    <h5 class="text-header-modal" id="exampleModalCenterTitle">Delete profile</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
                 <div class="modal-body">
-                    <p>¿Estás seguro que quieres eliminar este perfil? Una vez eliminado no lo podrás recuperar</p>
+                    <p>Are you sure you want to delete this profile? Once deleted you cannot recover it</p>
                 </div>
                 <div class="modal-footer border-0 d-flex justify-content-end">
                     <button @click="deleteProfile('Own')" type="button" class="btn" style="font-weight: 600;color:black;border-color: #DDE3F0;">
                         {{deleteText}}</button>
                     <button type="button" class="btn btn-primary" style="font-weight: 600;"
-                        data-dismiss="modal">Cancelar</button>
+                        data-dismiss="modal">Cancel</button>
                 </div>
             </div>
         </div>
@@ -253,19 +250,19 @@ template::headerSite('Dashboard del cliente');
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header  border-0">
-                    <h5 class="text-header-modal" id="exampleModalCenterTitle">Eliminar perfil compartido</h5>
+                    <h5 class="text-header-modal" id="exampleModalCenterTitle">Delete shared profile</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
                 <div class="modal-body">
-                    <p>¿Estás seguro que quieres eliminar este perfil? Perderás el acceso para verlo, pero el perfil seguirá disponible para el propietario.</p>
+                    <p>Are you sure you want to delete this profile? You will lose access to view it, but the profile will still be available to the owner.</p>
                 </div>
                 <div class="modal-footer  border-0 d-flex justify-content-end">
                     <button @click="deleteProfile('Shared')" type="button" class="btn" style="font-weight: 600;color:black;border-color: #DDE3F0;">
                         {{deleteText}}</button>
                     <button type="button" class="btn btn-primary" style="font-weight: 600;"
-                        data-dismiss="modal">Cancelar</button>
+                        data-dismiss="modal">Cancel</button>
                 </div>
             </div>
         </div>
